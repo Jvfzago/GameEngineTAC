4bf404ff65786a65d884d674ab929f72f7f4ffed
João Vitor Frabis Zago <90344892+Jvfzago@users.noreply.github.com>
2025-09-16 23:54:12 -0300

    Add files via upload

---
+++ Projeto TAC.IDJ/GameEngineTAC/src/State.cpp
@@ -0,0 +1,26 @@
+
+#include "state.h"
+
+State::State() : quitRequested(false), bg(nullptr){
+    music = Music("Recursos/audio/BGM.wav");
+}
+
+void State::LoadAssets(){
+    
+}
+
+void State::Update(float dt){
+    if(SDL_QuitRequested()){
+        quitRequested = true;
+    }
+}
+
+void State::Render(){
+    bg.Render(0,0);
+}
+
+bool State::QuitRequested(){
+    return quitRequested;
+}
+
+
